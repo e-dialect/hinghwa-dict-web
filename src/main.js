119871bac715405460031dabfa35cbb7bb30bb70
Frog Dar <1270610465@qq.com>
2021-08-13 16:14:33 +0800

    fix: token before axios request

--- src/main.js
+++ src/main.js
@@ -8,14 +8,17 @@ import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(Antd)
+
+// axios
 Vue.prototype.$axios = axios
 axios.defaults.baseURL = 'https://api.pxm.edialect.top/'
+
+/**
+ * axios请求拦截器
+ */
 axios.interceptors.request.use(function (config) {
   // Do something before request is sent
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.token = token // 将token放到请求头发送给服务器
-  }
+  config.headers.token = localStorage.getItem('token') || [] // 将token放到请求头发送给服务器
   return config
 }, function (error) {
   // Do something with request error
@@ -23,6 +26,9 @@ axios.interceptors.request.use(function (config) {
   return Promise.reject(error)
 })
 
+/**
+ * axios回应拦截器
+ */
 axios.interceptors.response.use(function (response) {
   // 对响应数据做点什么
   return response
