36e7cbe43b25f6f0dbefe65def9f819695ec9ce4
Frog Dar <1270610465@qq.com>
2021-08-16 16:32:37 +0800

    feat: Forbidden Page

--- src/views/Forbidden.vue
+++ src/views/Forbidden.vue
@@ -1,5 +1,16 @@
 <template>
-<a-spin>403</a-spin>
+  <a-result
+    status="403"
+    title="403"
+    sub-title="对不起，你没有访问该页面的权限！">
+    <template #extra>
+      <router-link :to="{name:'Home'}">
+        <a-button type="primary">
+          返回主页
+        </a-button>
+      </router-link>
+    </template>
+  </a-result>
 </template>
 
 <script>
