d15c81c6d2d76c77a28081013c7873b9e1b22fe5
Frog Dar <1270610465@qq.com>
2021-08-16 16:32:23 +0800

    feat: NotFound Page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,16 @@
 <template>
-<div>404</div>
+  <a-result
+    status="404"
+    title="404"
+    sub-title="对不起，你所访问的页面不存在！">
+    <template #extra>
+      <router-link :to="{name:'Home'}">
+        <a-button type="primary">
+          返回主页
+        </a-button>
+      </router-link>
+    </template>
+  </a-result>
 </template>
 
 <script>
