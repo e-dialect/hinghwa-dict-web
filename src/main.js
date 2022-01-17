f9795e310a1c7bf06aaab45b5c0c144e357b6a4a
sheeplin <1270610465@qq.com>
2022-01-17 17:59:05 +0800

    feat: add API cache

--- src/main.js
+++ src/main.js
@@ -13,12 +13,24 @@ Vue.use(Antd)
 Vue.prototype.$axios = axios
 axios.defaults.baseURL = 'https://api.pxm.edialect.top/'
 
+// 利用axios的cancelToken来取消请求
+const CancelToken = axios.CancelToken
+const cache = {}
 /**
  * axios请求拦截器
  */
 axios.interceptors.request.use(function (config) {
   // Do something before request is sent
   config.headers.token = localStorage.getItem('token') || [] // 将token放到请求头发送给服务器
+  if (config.cache) {
+    const source = CancelToken.source()
+    config.cancelToken = source.token
+    let data = cache[config.url]
+    if (config.params && cache[config.url]) { data = cache[config.url][JSON.stringify(config.params)] }
+    if (data && Date.now() - data.time < 60000) {
+      source.cancel(data)
+    }
+  }
   return config
 }, function (error) {
   // Do something with request error
@@ -31,8 +43,24 @@ axios.interceptors.request.use(function (config) {
  */
 axios.interceptors.response.use(function (response) {
   // 对响应数据做点什么
+
+  if (response.config.cache && response.config.method === 'get') {
+    // console.log(response.config)
+    const data = {
+      time: Date.now(),
+      data: response.data
+    }
+    if (response.config.params) {
+      if (!cache[response.config.url])cache[response.config.url] = {}
+      cache[response.config.url][JSON.stringify(response.config.params)] = data
+    } else { cache[response.config.url] = data }
+  }
   return response
 }, function (error) {
+  if (axios.isCancel(error)) {
+    // console.log(error)
+    return Promise.resolve(error.message)
+  }
   // 对响应错误做点什么
   switch (error.response.status) {
     case 401: {
