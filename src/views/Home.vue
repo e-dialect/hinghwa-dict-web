651ce5bf0e4c385183dbf19cde3d8851a8cacc05
123daxian <1905705430@qq.com>
2022-11-04 15:49:43 +0800

    feat: add cross word function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="body">
+  <div class="body" @click="SelectText($event)">
     <a-alert type="warning" show-icon>
       <template slot="message">
         网站移动版
@@ -47,7 +47,10 @@
         </div>
       </a-card>
     </a-row>
-
+<!--    划词功能-->
+    <div v-show="selectWordVisible" class="select-word" :style="{top:selectY,left:selectX}">
+      <li><span @click="search(selectedWord)">{{selectedWord}}</span></li>
+    </div>
     <a-row
       justify="space-around"
       style="padding-top:10px;padding-bottom:20px;padding-right:0;padding-left:0;"
@@ -94,7 +97,11 @@ export default {
           routerName: 'QuickRecording',
           disabled: false
         }
-      ]
+      ],
+      selectWordVisible: false,
+      selectedWord: '空',
+      selectX: '',
+      selectY: ''
     }
   },
   methods: {
@@ -107,8 +114,41 @@ export default {
       } else {
         this.$message.warning('请先输入搜索内容哦~')
       }
+    },
+    SelectText (e) {
+      const word = window.getSelection().toString().trim().replace(/\n/g, '') // 选中的内容
+      this.selectedWord = word
+      if (word !== '') {
+        setTimeout(() => {
+          this.selectWordVisible = true
+        }, 5000)
+        this.selectX = e.x + 'px'
+        this.selectY = e.y + 'px'
+      } else {
+        this.selectWordVisible = false
+      }
     }
   }
-
 }
 </script>
+<style>
+.select-word {
+  background: #fff;
+  position: fixed;
+  margin-top: -70px;
+  margin-left: -50px;
+  line-height: 42px;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #ccc;
+}
+li {
+  padding: 0 15px;
+  color: #8c8c8c;
+  list-style: none;
+}
+li:hover {
+  color: #417be0;
+  cursor: pointer;
+  background: #e7edfb;
+}
+</style>
