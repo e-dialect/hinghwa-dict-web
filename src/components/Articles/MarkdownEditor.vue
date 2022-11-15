dc138b5c61cef0bdde0bdd5ca1655524dcefcd19
sheeplin <1270610465@qq.com>
2022-11-15 16:31:34 +0800

    refactor: refactor with file services

--- src/components/Articles/MarkdownEditor.vue
+++ src/components/Articles/MarkdownEditor.vue
@@ -12,8 +12,7 @@
 <script>
 import { mavonEditor } from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
-import axios from 'axios'
-import { beforeUpload, imageUpload } from './ImageUpload'
+import { checkImageBeforeUpload, deleteFile, uploadFile } from '@/services/website'
 
 export default {
   name: 'MarkdownEditor',
@@ -43,43 +42,13 @@ export default {
   },
   methods: {
     /**
-     * 上传图片
-     * @param file 即将上传的图片文件
-     */
-    async imageUpload (file) {
-      return new Promise(resolve => {
-        var formdata = new FormData()
-        formdata.append('file', file)
-        axios({
-          url: '/website/files',
-          method: 'post',
-          data: formdata,
-          headers: { 'Content-Type': 'multipart/form-data' }
-        }).then((res) => {
-          resolve(res.data.url)
-          this.$message.success('成功上传啦~')
-        }).catch(err => {
-          this.$message.destroy()
-          switch (err.response.status) {
-            case 401: {
-              this.$message.error('无权限：请检查您的登录状态')
-              break
-            }
-            default: {
-              this.$message.error(err.toString())
-            }
-          }
-        })
-      })
-    },
-    /**
      * mavonEditor 绑定@imgAdd event
      * @param pos markdown编辑器中图片的位置
      * @param $file 用户上传的图片文件
      */
     $imgAdd (pos, $file) {
-      if (beforeUpload($file)) {
-        imageUpload($file).then(url => {
+      if (checkImageBeforeUpload($file)) {
+        uploadFile($file).then(url => {
           this.$refs.md.$img2Url(pos, url)
         })
       }
@@ -90,14 +59,11 @@ export default {
      * @param pos markdown编辑器中图片的位置
      */
     $imgDel (pos) {
-      axios.delete('/website/files', { data: { url: pos[0] } }).then(() => {
+      deleteFile({ url: pos[0] }).then(() => {
         this.$message.success('成功删除图片' + pos[1].name)
       }).catch(err => {
         this.$message.destroy()
-        console.log(pos[0])
-        console.log(pos[1])
-        console.log(err.response)
-        switch (err.response.status) {
+        switch (err.status) {
           case 401: {
             this.$message.error('无权限：请检查您的登录状态')
             break
