450c5fa0a98529ccc02c135c4a15354a827b3691
123daxian <106299221+123daxian@users.noreply.github.com>
2022-11-28 13:56:53 +0800

    fix: refactor quiz (#49)
    * refactor: refactor getQuiz
    
    * refactor: refactor searchquiz

---
+++ src/services/quiz.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+/**
+ * QZ0101 获取单个测试
+ * @param id 测试id
+ * @returns {Promise<unknown>}
+*/
+export function getQuiz (id) {
+  return request.get(`/quizzes/${id}`)
+}
+
+/**
+ * QZ0201 搜索指定的测试
+ * @param key 搜索关键词
+ * @returns {Promise<unknown>}
+ */
+export async function searchQuiz (key) {
+  return await request.get('/quizzes', { keywords: key })
+}
+
+/**
+ * QZ0202 获取随机的测试
+ * @returns {Promise<unknown>}
+ */
+export async function getRandomQuiz () {
+  return request.get('/quizzes/random')
+}
