8e0488c9ce8b65e8d3aa7b9d22c51e2957def0a4
123daxian <1905705430@qq.com>
2023-01-12 16:16:40 +0800

    feat: preparation for dictionary

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,6 +33,14 @@
       <a-card hoverable style="height:376px;text-align: center">
         <div style="text-align: right">
           <a-button
+            icon="file-search"
+            type="dashed"
+            @click="$router.push({name:'Dictionary'})"
+            style="margin-right: 8px"
+          >
+            进入辞典
+          </a-button>
+          <a-button
             icon="more"
             type="dashed"
             @click="$router.push({name:'WordDetails',params:{id:Math.ceil(Math.random()*3500)}})"
