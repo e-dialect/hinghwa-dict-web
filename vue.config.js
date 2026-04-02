6a64887edd0c11378159048b48b54581d3d6b0b4
lin594 <lin.yan@bupt.edu.cn>
2026-04-02 16:56:32 +0800

    build: disable roductionSourceMap

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // publicPath controls the base URL your application is deployed at.
   // For PR previews we set PREVIEW_BASE in CI (e.g. /pr-123/), otherwise default '/'.
   publicPath: process.env.PREVIEW_BASE || '/',
+  productionSourceMap: false,
   configureWebpack: config => {
     config.module.rules.push({
       test: /\.worker.js$/,
